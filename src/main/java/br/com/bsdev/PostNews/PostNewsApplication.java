faad3b81fd0b11cd471e7f15df9cffb7b61b13da
Blendo Santos <blendo@outlook.com>
2020-07-06 22:38:56 -0300

    Iniciando primeiro dia da Maratona Full Stack 1.0

---
+++ src/main/java/br/com/bsdev/PostNews/PostNewsApplication.java
@@ -0,0 +1,13 @@
+package br.com.bsdev.PostNews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostNewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostNewsApplication.class, args);
+	}
+
+}
